17e1d2a74a6a4337cce9cea1ce9f448b0f0b09af
minmmmmin <kotomi.hinahima@gmail.com>
2025-11-21 15:07:51 +0900

    マップのモック実装

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Map from "./components/Map"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-base-200">
       <Header />
+      <Map />
     </main>
   );
 }
