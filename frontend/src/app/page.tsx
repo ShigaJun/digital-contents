e94afcf66ed5e7baa5ea2a6833df84e11796e83a
minmmmmin <kotomi.hinahima@gmail.com>
2025-11-21 16:00:36 +0900

    タブの追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Map from "./components/Map"
+import TabsBar from "./components/TabsBar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-base-200">
       <Header />
       <Map />
+      <TabsBar />
     </main>
   );
 }
