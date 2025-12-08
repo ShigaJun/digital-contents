fb074b1875e3987549b142e347d156d2e3da147e
s5422053 <chda22053@g.nihon-u.ac.jp>
2025-12-08 22:39:55 +0900

    feature/30 URL/login にアクセスすることで認証ページにアクセスできるようになった

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { createClient } from '@/lib/supabase/client'
+import { useEffect, useState } from 'react'
+
+export default function AuthPage() {
+  const supabase = createClient()
+  const [redirectUrl, setRedirectUrl] = useState('');
+
+  useEffect(() => {
+    // ログイン後にリダイレクトしたいURLをここに設定します
+    // 例えば、現在のページのオリジンにリダイレクトしたい場合
+    setRedirectUrl(window.location.origin);
+  }, []);
+
+  if (!redirectUrl) {
+    return null; // redirectUrlが設定されるまでレンダリングしない
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
+        <h1 className="text-2xl font-bold text-center">Login / Register</h1>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={['google', 'github']} // 利用したいソーシャルプロバイダーを指定
+          redirectTo={`${redirectUrl}/auth/callback`}
+        />
+      </div>
+    </div>
+  )
+}
